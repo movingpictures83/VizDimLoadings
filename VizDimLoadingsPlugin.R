7f08750147b65b1a7538515098a87b221265caa3
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-05-20 09:06:31 -0400

    CSV test

--- VizDimLoadingsPlugin.R
+++ VizDimLoadingsPlugin.R
@@ -11,6 +11,7 @@ run <- function() {}
 
 output <- function(outputfile) {
  pdf(outputfile)
+write.csv(pbmc@commands$RunPCA.RNA@params$features, paste(outputfile, "csv", sep="."))
 VizDimLoadings(pbmc, dims = 1:2, reduction = "pca")
 }
 
